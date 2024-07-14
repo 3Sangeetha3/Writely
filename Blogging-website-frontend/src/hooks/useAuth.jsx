c90bba1c6732739019f07ef1a2d7658a061eba8d
sangeetha <jadamalsangeetha@gmail.com>
2024-07-14 17:25:49 +0530

    Added valtio library and implemented proxy state in useAuth hook

--- Blogging-website-frontend/src/hooks/useAuth.jsx
+++ Blogging-website-frontend/src/hooks/useAuth.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import { json } from 'react-router-dom';
+import { proxy, useSnapshot } from 'valtio';
+import { proxyWithComputed } from 'valtio/utils';
+
+function getAuthUser() {
+    const jwt = window.localStorage.getItem('jwtToken');
+
+    if(!jwt) return {};
+    return JSON.parse(atob(jwt));
+}
+//proxy object
+const state = proxy (
+    {
+        authUser: getAuthUser(),
+    },
+    {
+        isAuth: true
+    }
+);
+
 
 const actions = {
     login: (user) => {
-        console.log('login', {user})
+        console.log('login', {user, state});
+        state.authUser = user;
+        window.localStorage.setItem('jwtToken', btoa(JSON.stringify(state.authUser)))
     }
 }
+
+
 function useAuth() {
-  return {
-    ...actions 
-  }
+    const snap = useSnapshot(state);
+    console.log('snap: ', {snap});
+    return {
+        ...snap,
+        ...actions
+    }
 }
 
 export default useAuth;
