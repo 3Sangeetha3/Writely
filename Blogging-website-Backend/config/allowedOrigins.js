8272cb8f855d4e727661fd1fb08a9d8537d7d66f
sangeetha <jadamalsangeetha@gmail.com>
2024-07-18 20:41:46 +0530

    Update allowed origins to include new domain for backend API

--- Blogging-website-Backend/config/allowedOrigins.js
+++ Blogging-website-Backend/config/allowedOrigins.js
@@ -2,7 +2,6 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://blogging-website-5l8x.onrender.com',
-    'https://blogging-website-sepia.vercel.app'
 ]
 
 module.exports = allowedOrigins;
