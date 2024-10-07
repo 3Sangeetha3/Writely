fbf87119df23f2f60c920446f9c20dbca12f3bc7
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2024-10-07 21:04:00 +0530

    Update allowedOrigins.js with new domain for writely.3sangeetha3.me

--- Blogging-website-Backend/config/allowedOrigins.js
+++ Blogging-website-Backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:5173/*',
     'http://localhost:5174/*',
     'https://blogging-website-sepia.vercel.app/*',
-    'https://blogging-website.3sangeetha3.me/*'
+   " https://writely.3sangeetha3.me/*"
 ]
 
 module.exports = allowedOrigins;
