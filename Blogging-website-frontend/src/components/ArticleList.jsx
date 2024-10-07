7de6afa694c13becda77555d89f1a689c5c6d4db
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2024-10-07 13:46:43 +0530

    Implement: scrolling animations in feed(AOS)

--- Blogging-website-frontend/src/components/ArticleList.jsx
+++ Blogging-website-frontend/src/components/ArticleList.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { isEmpty, isNil } from 'lodash-es'
-import { useArticlesQuery } from '../hooks'
-import ArticlePreview from './ArticlePreview'
-
-
+import React from "react";
+import { isEmpty, isNil } from "lodash-es";
+import { useArticlesQuery } from "../hooks";
+import ArticlePreview from "./ArticlePreview";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function ArticleList() {
-  const { articles } = useArticlesQuery()
+  const { articles } = useArticlesQuery();
+
+  // Initialize AOS on component mount
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Duration of animation (in ms)
+      easing: "ease-in-out", // Easing effect
+      // once: true, // Whether animation happens only once
+    });
+  }, []);
 
   // console.log('ArticleList',{articles})
 
@@ -18,12 +28,19 @@ function ArticleList() {
 
   return (
     <>
-      {articles.map((article) => (
-        <ArticlePreview key={article.slug} article={article} />
+      {articles.map((article, index) => (
+        <div
+          key={article.slug}
+          data-aos="fade-up"
+          data-aos-duration="1500"
+          data-aos-easing="ease-in-out"
+          data-aos-delay={`${index * 100}`} // Optional: adds delay to stagger animations
+        >
+          <ArticlePreview article={article} />
+        </div>
       ))}
-
     </>
-  )
+  );
 }
 
-export default ArticleList
+export default ArticleList;
