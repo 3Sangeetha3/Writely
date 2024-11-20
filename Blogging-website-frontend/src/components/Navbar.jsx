78739751da3bf2f37576f2f9110ee85c6be88bc3
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2024-11-21 00:24:07 +0530

    Add icons for navigation in Navbar component

--- Blogging-website-frontend/src/components/Navbar.jsx
+++ Blogging-website-frontend/src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useAuth } from "../hooks";
 import { useNavigate } from "react-router-dom"; // Import useNavigate for programmatic navigation
 import Logo from "../assets/Writely_logo.svg";
+import NewPostIcon from "../assets/new_post_icon.svg";
+import HomeIcon from "../assets/Home_icon.svg";
+import SettingsIcon from "../assets/settings.svg";
 
 function Navbar() {
   const { isAuth, authUser } = useAuth();
@@ -17,13 +20,13 @@ function Navbar() {
         <div className="flex items-center cursor-pointer" onClick={() => handleNavigation("/")}>
           <img src={Logo} alt="Writely Logo" className="w-44 h-auto" />
         </div>
-        <ul className="flex space-x-10">
+        <ul className="flex space-x-2">
           <li className="text-2xl">
             <button
               className="text-gray-600 no-underline cursor-pointer hover:text-[#475756] hover:bg-[#E6E8E5] transition duration-300 p-2 rounded font-semibold"
               onClick={() => handleNavigation("/")}
             >
-              Home
+              <img src={HomeIcon} alt="Home icon" className="w-8 h-8" />
             </button>
           </li>
           {isAuth && (
@@ -33,7 +36,7 @@ function Navbar() {
                   className="text-gray-600 no-underline cursor-pointer hover:text-[#475756] hover:bg-[#E6E8E5] transition duration-300 p-2 rounded font-semibold"
                   onClick={() => handleNavigation("/editor")}
                 >
-                  New Post
+                  <img src={NewPostIcon} alt="New post icon" className="w-8 h-8" />
                 </button>
               </li>
               <li className="text-2xl">
@@ -41,7 +44,7 @@ function Navbar() {
                   className="text-gray-600 no-underline cursor-pointer hover:text-[#475756] hover:bg-[#E6E8E5] transition duration-300 p-2 rounded font-semibold"
                   onClick={() => handleNavigation("/settings")}
                 >
-                  Settings
+                  <img src={SettingsIcon} alt="Settings icon" className="w-8 h-8" />
                 </button>
               </li>
               <li className="text-2xl">
