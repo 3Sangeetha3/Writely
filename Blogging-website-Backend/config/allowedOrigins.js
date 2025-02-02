903e4c28130b851b426bb17189cdb9fe5d03f7ef
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2025-02-02 13:27:57 +0530

    update: change allowed origin URL for the blogging website

--- Blogging-website-Backend/config/allowedOrigins.js
+++ Blogging-website-Backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
-    'https://blogging-website-3sangeetha3s-projects.vercel.app',
+    'https://blogging-website-git-main-3sangeetha3s-projects.vercel.app/',
     "https://writely.3sangeetha3.me"
 ]
 
