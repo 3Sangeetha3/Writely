8acd567bf1ba48ba2040d4a84d7ccd61c1e74836
Jadamal Sangeetha Choudhary <jadamalsangeetha@lpu.in>
2025-03-07 21:16:33 +0530

    update: Fix trailing slash in allowedOrigins for Writely URL

--- Blogging-website-Backend/config/allowedOrigins.js
+++ Blogging-website-Backend/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://localhost:5173/',
     'http://localhost:5174',
     'https://blogging-website-git-main-3sangeetha3s-projects.vercel.app/',
-    "https://writely.3sangeetha3.tech",
+    "https://writely.3sangeetha3.tech/",
     "https://blogging-website-eo1.pages.dev/"
 
 ]
